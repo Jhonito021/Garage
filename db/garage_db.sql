a522c10e83469ac71caf9467c6c1805fcd0623f6
Jhonito021 <jhonito021@gmail.com>
2026-05-29 08:46:34 +0300

    CahierDesNorme

--- db/garage_db.sql
+++ db/garage_db.sql
@@ -1,27 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 28 mai 2026 à 08:34
--- Version du serveur : 9.1.0
--- Version de PHP : 8.4.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `garage_db`
---
-
--- --------------------------------------------------------
 
 --
 -- Structure de la table `configurations`
